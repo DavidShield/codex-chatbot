15448d9884705e0efc862617f93908801cc29a3b
Dawei Wang <wangdawei@microsoft.com>
2023-02-27 21:01:27 -0800

    first commit

--- app.py
+++ app.py
@@ -1,35 +1,42 @@
 import os
 
 import openai
-from flask import Flask, redirect, render_template, request, url_for
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
+app.static_folder = 'static'
+
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
+@app.route("/")
+def home():
+    return render_template("index.html")
 
-@app.route("/", methods=("GET", "POST"))
-def index():
-    if request.method == "POST":
-        animal = request.form["animal"]
-        response = openai.Completion.create(
-            model="text-davinci-003",
-            prompt=generate_prompt(animal),
-            temperature=0.6,
-        )
-        return redirect(url_for("index", result=response.choices[0].text))
+@app.route("/get")
+def get_bot_response():
+    userText = request.args.get('msg')
+    return getResponse(userText)
 
-    result = request.args.get("result")
-    return render_template("index.html", result=result)
+start_sequence = "\nAI:"
+restart_sequence = "\nHuman: "
+prompt="The following is a conversation with an Microsoft Azure ARM API assistant. The assistant will not complete the question. If the question is specific enough, the assistant will  answer the question by providing azure arm api endpoint and the complete endpoint request json format. Otherwise, it keep asking the question to make it clear.\n\nHuman: Hello, who are you?\nAI: Hi there! I'm an Azure ARM API assistant. I can help you with questions related to Azure ARM API endpoints and request JSON formats. How can I help you?\nHuman: Create a resource group.\nAI: Sure, I can help you with that. Can you provide me with the name of the resource group you'd like to create?\nHuman: wangdawei-rg\nAI: Got it. To create a resource group with the name 'wangdawei-rg', you can use the following Azure ARM API endpoint: PUT https://management.azure.com/subscriptions/{subscriptionId}/resourceGroups/{resourceGroupName}?api-version=2019-10-01. JSON:\n\n{\n    \"location\": \"<location>\",\n    \"properties\": \n        \"provisioningState\": \"Succeeded\"\n    }\n}"
 
-
-def generate_prompt(animal):
-    return """Suggest three names for an animal that is a superhero.
-
-Animal: Cat
-Names: Captain Sharpclaw, Agent Fluffball, The Incredible Feline
-Animal: Dog
-Names: Ruff the Protector, Wonder Canine, Sir Barks-a-Lot
-Animal: {}
-Names:""".format(
-        animal.capitalize()
+def getResponse(msg):
+    global prompt
+    prompt += " Human:" + msg
+    response = openai.Completion.create(
+        model="text-davinci-003",
+        prompt=prompt,
+        temperature=0,
+        max_tokens=1000,
+        top_p=1,
+        frequency_penalty=0,
+        presence_penalty=0.6,
+        stop=[" Human:", "AI:"]
     )
+    prompt += response.choices[0].text
+    print(response.choices[0].text)
+    return response
+
+if __name__ == "__main__":
+    app.run()
